9943126824677bd206c38adacfaa04464b5777b2
AwsAlyasjen <awsalyasjen@gmail.com>
2022-01-30 10:20:32 +0200

    update

--- Challenge07/Challenge07.js
+++ Challenge07/Challenge07.js
@@ -19,7 +19,6 @@
 // ------------------------
 
 const objLat = (obj) => {
-    // write your code here
     var firstName = obj.firstName[0].toUpperCase() + obj.firstName.slice(1, obj.firstName.length);
     var lastName = obj.lastName[0].toUpperCase() + obj.lastName.slice(1, obj.lastName.length);
 
@@ -87,30 +86,30 @@ const objLat = (obj) => {
 
 // ------------------------
 const cvFormatter = (arr) => {
-    let data = [];
+    let form = [];
 
     for (let i = 0; i < arr.length; i++) {
-        const betaData = arr[i];
-        if (betaData.yearsOfExperience > 1) {
+        const x = arr[i];
+        if (x.yearsOfExperience > 1) {
             var fullName = "";
 
-            if (betaData.firstName && betaData.firstName !== "null")
-                fullName = betaData.firstName;
+            if (x.firstName && x.firstName !== "null")
+                fullName = x.firstName;
 
-            if (betaData.lastName && betaData.lastName !== "null") {
-                if (betaData.firstName && betaData.firstName !== "null")
+            if (x.lastName && x.lastName !== "null") {
+                if (x.firstName && x.firstName !== "null")
                     fullName += " ";
-                fullName += betaData.lastName;
+                fullName += x.lastName;
             }
 
-            data.push({
+            form.push({
                 fullName: fullName,
-                tech: betaData.tech
+                tech: x.tech
             });
         }
     }
 
-    return data;
+    return form;
 };
 
 // 3) ---------------------
@@ -134,6 +133,7 @@ const cvFormatter = (arr) => {
 
 // ------------------------
 const applicationsStatics = (arr) => {
+    // write your code here
     let result = {
         python_Devs: 0,
         javaScript_Devs: 0,
@@ -144,26 +144,23 @@ const applicationsStatics = (arr) => {
     }
 
     for (let i = 0; i < arr.length; i++) {
-        const alphaData = arr[i];
-        if (alphaData.tech === "Python") {
+        const x = arr[i];
+        if (x.tech === "Python") {
             result.python_Devs++;
         }
-        else if (alphaData.tech === "JS") {
+        else if (x.tech === "JS") {
             result.javaScript_Devs++;
         }
 
-        else if (alphaData.tech === ".Net") {
+        else if (x.tech === ".Net") {
             result.dotNet_Devs++;
         }
-        else if (alphaData.tech === "Java") {
+        else if (x.tech === "Java") {
             result.java_Devs++;
         }
 
-        if (alphaData.yearsOfExperience <= 1 ||
-             (alphaData.firstName == "null" ||
-              alphaData.firstName == "") && 
-              (alphaData.lastName ==
-            "null" || alphaData.lastName == "")) {
+        if (x.yearsOfExperience <= 1 || (x.firstName == "null" || x.firstName == "") && (x.lastName ==
+            "null" || x.lastName == "")) {
             result.rejectedApplicants++
 
         }
@@ -172,21 +169,137 @@ const applicationsStatics = (arr) => {
     return result;
 };
 
+// 4) ---------------------
+//
+//  A Certain School principal wants to calculate the average score of each class in each grade in the school
+//  so he requested a programmer to solve this issue for him instead of doing it again every time
+//
+//  given the school data, calculate the average score of the students of each class in each grade
+//  then change the avg value for the class
+//  EX:
+
+let data = {
+    SchoolName: "David Academy",
+    Capacity: 1000,
+    grades: [
+        {
+            grade: "First",
+            numberOFClasses: 3,
+            classes: [
+                {
+                    avg: 0,
+                    classNumber: "01",
+                    classScores: [
+                        89, 87, 45, 48, 89, 65, 21, 54, 78, 62, 51, 54, 50, 49
+                    ]
+                },
+                {
+                    avg: 0,
+                    classNumber: "02",
+                    classScores: [
+                        87, 54, 95, 45, 41, 51, 25, 63, 58, 47, 64, 51, 98, 100
+                    ]
+                },
+                {
+                    avg: 0,
+                    classNumber: "03",
+                    classScores: [
+                        74, 85, 20, 51, 59, 58, 62, 71, 74, 99, 84, 71, 38, 70
+                    ]
+                },
+            ]
+        },
+        {
+            grade: "Second",
+            numberOFClasses: 2,
+            classes: [
+                {
+                    avg: 0,
+                    classNumber: "01",
+                    classScores: [
+                        71, 82, 86, 3, 1, 52, 91, 84, 44, 68, 64, 23, 80, 60,
+                    ]
+                },
+                {
+                    avg: 0,
+                    classNumber: "02",
+                    classScores: [
+                        86, 39, 71, 17, 61, 76, 60, 54, 42, 65, 49, 75, 96, 70
+                    ]
+                },
+            ]
+        },
+        {
+            grade: "Third",
+            numberOFClasses: 2,
+            classes: [
+                {
+                    avg: 0,
+                    classNumber: "01",
+                    classScores: [
+                        11, 12, 28, 78, 57, 3, 54, 72, 14, 19, 88, 74, 68, 58
+                    ]
+                },
+                {
+                    avg: 0,
+                    classNumber: "02",
+                    classScores: [
+                        62, 8, 35, 43, 96, 9, 97, 73, 4, 32, 18, 81, 70, 42
+                    ]
+                },
+            ]
+        },
+        {
+            grade: "Forth",
+            numberOFClasses: 4,
+            classes: [
+                {
+                    avg: 0,
+                    classNumber: "01",
+                    classScores: [
+                        63, 71, 93, 87, 83, 85, 67, 49, 62, 45, 38, 48, 58, 52
+                    ]
+                },
+                {
+                    avg: 0,
+                    classNumber: "02",
+                    classScores: [
+                        100, 45, 70, 75, 87, 63, 39, 46, 54, 68, 74, 96, 52, 49
+                    ]
+                },
+                {
+                    avg: 0,
+                    classNumber: "03",
+                    classScores: [
+                        97, 54, 80, 95, 69, 31, 88, 84, 50, 81, 67, 34, 41, 87
+                    ]
+                },
+                {
+                    avg: 0,
+                    classNumber: "04",
+                    classScores: [
+                        64, 96, 66, 38, 78, 58, 43, 100, 34, 56, 82, 53, 89, 72
+                    ]
+                },
+            ]
+        },
+    ],
+};
+
 //  Note that:
 //  1- This is not the exact data you will be getting every time and the solution should be dynamic
 //  2- You need to round the average to the nearest lower number 
 
 const classesAvg = (data) => {
     for (let i = 0; i < data.grades.length; i++) {
-        const marks = data.grades[i];
-    for (let j = 0; j < marks.classes.length; j++) {
-        let sum = 0;   
-        const newMarks = marks.classes[j];
-          
+        const grade = data.grades[i];
+        for (let j = 0; j < grade.classes.length; j++) {
+            const x = grade.classes[j];
+            let sum = 0;
 
-            for (let x = 0; x < newMarks.classScores.length; x++)
-                sum += newMarks.classScores[x];
-            newMarks.avg = Math.floor(sum / newMarks.classScores.length);
+            for (let q = 0; q < x.classScores.length; q++)
+                sum += x.classScores[q];
+            x.avg = Math.floor(sum / x.classScores.length);
         }
     }
     return data;
